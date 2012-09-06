abb63469e8aac519979e48a8e7558a2b3f45999c
jmorille <jmorille@generali.fr>
2012-09-06 23:18:23 +0200

    edit person

--- src/eu/ttbox/smstraker/ui/person/AddPersonActivity.java
+++ src/eu/ttbox/smstraker/ui/person/AddPersonActivity.java
@@ -1,16 +1,22 @@
 package eu.ttbox.smstraker.ui.person;
 
-import eu.ttbox.smstraker.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.EditText;
+import eu.ttbox.smstraker.R;
 
 public class AddPersonActivity extends Activity {
 
+    private EditText nameEditText;
+    private EditText phoneEditText;
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_person);
+        // binding
+        nameEditText = (EditText)findViewById(R.id.person_name);
+        phoneEditText = (EditText)findViewById(R.id.person_phone);
     }
     
     
