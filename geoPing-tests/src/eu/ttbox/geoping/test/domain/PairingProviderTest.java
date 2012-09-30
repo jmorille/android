bbdb485fe84fd722d6667a02fbac898387bb83ba
jmorille <jmorille@generali.fr>
2012-09-30 21:09:46 +0200

    Test

--- geoPing-tests/src/eu/ttbox/geoping/test/domain/PairingProviderTest.java
+++ geoPing-tests/src/eu/ttbox/geoping/test/domain/PairingProviderTest.java
@@ -33,7 +33,7 @@ public class PairingProviderTest extends ProviderTestCase2<PairingProvider> {
         }
     }
 
-    public void testInsertandRead() {
+    public void testInsertAndRead() {
         ContentProvider provider = getProvider();
         // Data
         Pairing vo = new Pairing()//
@@ -42,7 +42,7 @@ public class PairingProviderTest extends ProviderTestCase2<PairingProvider> {
                 .setAuthorizeType(PairingAuthorizeTypeEnum.AUTHORIZE_ALWAYS);
         ContentValues values = PairingHelper.getContentValues(vo);
         // Insert
-        Uri uri = provider.insert( CONTENT_URI, values);
+        Uri uri = provider.insert(CONTENT_URI, values);
         // Select
         Cursor cursor = provider.query(uri, null, null, null, null);
         try {
@@ -56,6 +56,36 @@ public class PairingProviderTest extends ProviderTestCase2<PairingProvider> {
             assertEquals(vo.authorizeType, voDb.authorizeType);
         } finally {
             cursor.close();
+        }
+    }
+
+    public void testInsertAndSearchForPhone() {
+        ContentProvider provider = getProvider();
+        // Data
+        Pairing vo = new Pairing()//
+                .setPhone("0601020304") //
+                .setShowNotification(true)//
+                .setAuthorizeType(PairingAuthorizeTypeEnum.AUTHORIZE_ALWAYS);
+        ContentValues values = PairingHelper.getContentValues(vo);
+        // Insert
+        Uri uri = provider.insert(CONTENT_URI, values);
+        // Select
+        Uri searchPhoneUri01 = Uri.withAppendedPath(PairingProvider.Constants.CONTENT_URI_PHONE_FILTER, Uri.encode("+33601020304"));
+        Uri searchPhoneUri02 = Uri.withAppendedPath(PairingProvider.Constants.CONTENT_URI_PHONE_FILTER, Uri.encode("0601020304"));
+        for (Uri searchPhoneUri : new Uri[] { searchPhoneUri01, searchPhoneUri02 }) {
+            Cursor cursor = provider.query(searchPhoneUri, null, null, null, null);
+            try {
+                assertEquals(1, cursor.getCount());
+                cursor.moveToFirst();
+                PairingHelper helper = new PairingHelper().initWrapper(cursor);
+                Pairing voDb = helper.getEntity(cursor);
+                assertEquals(Long.valueOf(uri.getLastPathSegment()).longValue(), voDb.id);
+                assertEquals(vo.phone, voDb.phone);
+                assertEquals(vo.showNotification, voDb.showNotification);
+                assertEquals(vo.authorizeType, voDb.authorizeType);
+            } finally {
+                cursor.close();
+            }
         }
     }
 
