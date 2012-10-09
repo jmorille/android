4c8745e74d01f9757aafb869c8aabe96d920a05f
jmorille <jmorille@generali.fr>
2012-10-09 17:21:15 +0200

    src

--- src/eu/ttbox/geoping/ui/widget/GeoPingSenderWidgetProvider.java
+++ src/eu/ttbox/geoping/ui/widget/GeoPingSenderWidgetProvider.java
@@ -11,46 +11,43 @@ import android.content.Intent;
 import android.util.Log;
 import android.widget.RemoteViews;
 import eu.ttbox.geoping.R;
+
 /**
  * {link http://www.vogella.com/articles/AndroidWidgets/article.html}
+ * 
  * @author jmorille
- *
+ * 
  */
-public class GeoPingSenderWidgetProvider extends AppWidgetProvider  {
+public class GeoPingSenderWidgetProvider extends AppWidgetProvider {
 
-    private static final String ACTION_CLICK = "ACTION_CLICK";
-    
-    @Override
-    public void onUpdate(Context context, AppWidgetManager appWidgetManager,
-        int[] appWidgetIds) {
-        // Get all ids
-        ComponentName thisWidget = new ComponentName(context,
-                GeoPingSenderWidgetProvider.class);
-        int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
-        for (int widgetId : allWidgetIds) {
-          // Create some random data
-          int number = (new Random().nextInt(100));
+	private static final String ACTION_CLICK = "ACTION_CLICK";
 
-          RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
-              R.layout.widget_geoping_sender);
-          Log.w("WidgetExample", String.valueOf(number));
-          // Set the text
-          remoteViews.setTextViewText(R.id.update, String.valueOf(number));
+	@Override
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+		// Get all ids
+		ComponentName thisWidget = new ComponentName(context, GeoPingSenderWidgetProvider.class);
+		int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
+		for (int widgetId : allWidgetIds) {
+			// Create some random data
+			int number = (new Random().nextInt(100));
 
-          // Register an onClickListener
-          Intent intent = new Intent(context, GeoPingSenderWidgetProvider.class);
+			RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_geoping_sender);
+			Log.w("WidgetExample", String.valueOf(number));
+			// Set the text
+			remoteViews.setTextViewText(R.id.update, String.valueOf(number));
 
-          intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
-          intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+			// Register an onClickListener
+			Intent intent = new Intent(context, GeoPingSenderWidgetProvider.class);
 
-          PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
-              0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-          remoteViews.setOnClickPendingIntent(R.id.update, pendingIntent);
-          appWidgetManager.updateAppWidget(widgetId, remoteViews);
-        }
-        // Super
-        super.onUpdate(context, appWidgetManager, appWidgetIds);
-    }
-    
-    
+			intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+			intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+
+			PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+			remoteViews.setOnClickPendingIntent(R.id.update, pendingIntent);
+			appWidgetManager.updateAppWidget(widgetId, remoteViews);
+		}
+		// Super
+		super.onUpdate(context, appWidgetManager, appWidgetIds);
+	}
+
 }
