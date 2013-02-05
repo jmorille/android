b3349a9d02f2f05a9b26671d2a132c8ac9af283e
jmorille <jmorille@gmail.com>
2013-02-05 22:55:44 +0100

    Backup

--- geoPing/src/eu/ttbox/geoping/service/backup/PairingBackupHelper.java
+++ geoPing/src/eu/ttbox/geoping/service/backup/PairingBackupHelper.java
@@ -18,8 +18,8 @@ import eu.ttbox.geoping.domain.pairing.PairingDatabase;
 import eu.ttbox.geoping.domain.pairing.PairingDatabase.PairingColumns;
 
 /**
- * http://blog.stylingandroid.com/archives/781 
- *
+ * http://blog.stylingandroid.com/archives/781
+ * 
  */
 public class PairingBackupHelper implements BackupHelper {
 
@@ -30,7 +30,7 @@ public class PairingBackupHelper implements BackupHelper {
 	private Context context;
 	private PairingDatabase pairingDatabase;
 
-	public PairingBackupHelper(Context ctx, String dbName) {
+	public PairingBackupHelper(Context ctx) {
 		super();
 		this.context = ctx;
 		this.pairingDatabase = new PairingDatabase(ctx);
@@ -49,15 +49,15 @@ public class PairingBackupHelper implements BackupHelper {
 		// Doing copy
 		copyTable(stringColums, intColums, longColums);
 		// Out Values
-//		ByteArrayOutputStream bufStream = new ByteArrayOutputStream();
-//		DataOutputStream outWriter = new DataOutputStream(bufStream);
-//		
-//		 byte[] stringBytes = mStringToBackUp.getBytes();
-//		       data.writeEntityHeader(DB_PAIRING_KEY, stringBytes.length);
-//		     data.writeEntityData(stringBytes, stringBytes.length);
+		// ByteArrayOutputStream bufStream = new ByteArrayOutputStream();
+		// DataOutputStream outWriter = new DataOutputStream(bufStream);
+		//
+		// byte[] stringBytes = mStringToBackUp.getBytes();
+		// data.writeEntityHeader(DB_PAIRING_KEY, stringBytes.length);
+		// data.writeEntityData(stringBytes, stringBytes.length);
 	}
 
-	public void copyTable(String[] stringColums, String[] intColums, String[] longColums) { 
+	public void copyTable(String[] stringColums, String[] intColums, String[] longColums) {
 		// Do copy Table
 		Cursor cursor = null;
 		try {
@@ -78,13 +78,17 @@ public class PairingBackupHelper implements BackupHelper {
 
 	@Override
 	public void restoreEntity(BackupDataInputStream data) {
-		 
-		Log.i(TAG, "----- onRestore End"); 
+		String key = data.getKey();
+		Log.d(TAG, "got entity '" + key + "' size=" + data.size());
+//		 if (isKeyInList(key, mPrefGroups)) {
+//			 
+//		 }
+		Log.i(TAG, "----- onRestore End");
 	}
 
 	@Override
 	public void writeNewStateDescription(ParcelFileDescriptor newState) {
-	 
+
 	}
 
 }
