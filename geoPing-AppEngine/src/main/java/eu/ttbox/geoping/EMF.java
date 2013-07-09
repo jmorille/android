e6d12e89bdd72eb6c04461cb6c96bd8cf9440649
jmorille <jmorille@gmail.com>
2013-07-09 17:50:06 +0200

    rest api

---
+++ geoPing-AppEngine/src/main/java/eu/ttbox/geoping/EMF.java
@@ -0,0 +1,16 @@
+package eu.ttbox.geoping;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public final class EMF {
+    private static final EntityManagerFactory INSTANCE =
+            Persistence.createEntityManagerFactory("transactions-optional");
+
+    private EMF() {
+    }
+
+    public static EntityManagerFactory get() {
+        return INSTANCE;
+    }
+}
