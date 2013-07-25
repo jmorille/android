c905367f54db8e9a1108467f8cb479440e318475
jmorille <jmorille@gmail.com>
2013-07-25 18:12:25 +0200

    doc

---
+++ geoPingProject/geoPing/src/main/java/eu/ttbox/geoping/ui/billing/SkuProductEnum.java
@@ -0,0 +1,16 @@
+package eu.ttbox.geoping.ui.billing;
+
+
+public enum SkuProductEnum {
+
+    SKU_NO_AD_PER_YEAR  ("no_ad_per_year");;
+
+    public final String sku;
+
+    SkuProductEnum(String sku) {
+        this.sku = sku;
+    }
+
+
+
+}
